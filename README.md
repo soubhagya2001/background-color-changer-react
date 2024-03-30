6e94c8a21d7ce68a2eba220fe31e86dd9e77afa2
soubhagya2001 <soubhagyaprusty36@gmail.com>
2024-03-30 11:21:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Background Color Changer App
+
+This is a simple background color changer app built using React and Vite. It allows users to change the background color of the app by selecting different color options.
+
+## Installation
+
+To run this application locally, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/color-picker-app.git
+
+2. Navigate to the project directory:
+    ```bash
+    cd color-picker-app
+
+3. Install dependencies using npm or yarn:
+    ```bash
+    npm install
+# or
+    yarn install
+
+4. Start the development server:
+    ```bash
+    npm run dev
+# or
+yarn dev
+
+5. Open your browser and navigate to http://localhost:3000 to view the app.
+
+
+## Usage
+Once the app is running, you can click on the color buttons to change the background color of the app. The available colors are Red, Green, Blue, Olive, Gray, and Yellow.
+
+## Technologies Used
+React: A JavaScript library for building user interfaces.
+Vite: A fast development build tool for modern web applications.
+Tailwind CSS: A utility-first CSS framework for rapidly building custom designs.
+
+## Folder Structure
+The project structure is organized as follows:
+
+src: Contains the source code of the React components and styles.
+App.js: Main component that renders the color picker buttons.
+index.html: HTML entry point for the application.
+README.md: Documentation for the project.
+package.json: Configuration file for npm dependencies and scripts.
+
+
---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div
+          className="fixed flex flex-wrap justify-center 
+        bottom-12 inset-x-0 px-2"
+        >
+          <div
+            className="flex flex-wrap justify-center gap-3 
+          shadow-lg bg-white px-3 py-2 rounded-3xl"
+          >
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              RED
+            </button>
+
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+            >
+              GREEN
+            </button>
+
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+            >
+              BLUE
+            </button>
+
+            <button
+              onClick={() => setColor("olive")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "olive" }}
+            >
+              OLIVE
+            </button>
+
+            <button
+              onClick={() => setColor("gray")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "gray" }}
+            >
+              GRAY
+            </button>
+
+            <button
+              onClick={() => setColor("yellow")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+            >
+              YELLOW
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
